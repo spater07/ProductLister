a66ab626a8bc6df6f1e0a22e240b92103b9a38d2
Chetan Pardhi <chetanpardhi@Inspiron-15-3567-edff6409.localdomain>
2022-03-13 07:55:15 +0000

    message added

--- bestdeals.py
+++ bestdeals.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/scrap-data', methods=['GET'])
 def scrap_data():
-    return 'Hello from scrappy service, nice to meet you.'
+    return 'Hello from scrappy service, nice to meet you.Hii chetan'
     
 if __name__ == '__main__':
     app.run()
